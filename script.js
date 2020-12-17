0881c4d0b77b9f10dd4fba41c0c62f56f8ededde
iskender-jumagulov <jiskender1@gmail.com>
2020-12-17 09:01:32 +0600

    new files

--- script.js
+++ script.js
@@ -18,63 +18,30 @@ window.onscroll = function(){
         navFix[0].style.position = "static";
         navHide[0].style.position = "static";
     }
-}
-// Анимация текстов
-// let animSection1 = document.getElementsByClassName('section1');
-//     animSection1[0].style.opacity = "0";
-//     animSection1[0].style.transform = "translateX(-125px)";
-//     animSection1[0].style.transition = "transform 0.85s cubic-bezier(0.455,  0.030, 0.515, 0.955) 0.2s, opacity 0.85s cubic-bezier(0.455,  0.030, 0.515, 0.955) 0.2s";
-    
-// let animSection2 = document.getElementsByClassName('section2');
-//     animSection2[0].style.opacity = "0";
-//     animSection2[0].style.transform = "translateX(-125px)";
-//     animSection2[0].style.transition = "transform 0.85s cubic-bezier(0.455,  0.030, 0.515, 0.955) 0.2s, opacity 0.85s cubic-bezier(0.455,  0.030, 0.515, 0.955) 0.2s";
+};
 
-// let animSection3 = document.getElementsByClassName('section3');
-//     animSection3[0].style.opacity = "0";
-//     animSection3[0].style.transform = "translateX(-125px)";
-//     animSection3[0].style.transition = "transform 0.85s cubic-bezier(0.455,  0.030, 0.515, 0.955) 0.2s, opacity 0.85s cubic-bezier(0.455,  0.030, 0.515, 0.955) 0.2s";
+//Слайдер
 
-// let animSection4 = document.getElementsByClassName('section4');
-//     animSection4[0].style.opacity = "0";
-//     animSection4[0].style.transform = "translateX(-125px)";
-//     animSection4[0].style.transition = "transform 0.85s cubic-bezier(0.455,  0.030, 0.515, 0.955) 0.2s, opacity 0.85s cubic-bezier(0.455,  0.030, 0.515, 0.955) 0.2s";
+let slidesPartners = document.getElementsByClassName('slide');
+let slidesArrow = document.getElementsByClassName('slides-arrow');
 
-// let animSection5 = document.getElementsByClassName('section5');
-//     animSection5[0].style.opacity = "0";
-//     animSection5[0].style.transform = "translateX(-125px)";
-//     animSection5[0].style.transition = "transform 0.85s cubic-bezier(0.455,  0.030, 0.515, 0.955) 0.2s, opacity 0.85s cubic-bezier(0.455,  0.030, 0.515, 0.955) 0.2s";
-    
-//     let pos = pageYOffset; // Переменная получает значение Y
-    
-//     if(pos > -50){
-//         animSection1[0].style.opacity = "1";
-//         animSection1[0].style.transform = "translateX(0)";
-//         animSection1[0].style.transition = "transform 0.85s cubic-bezier(0.455,  0.030, 0.515, 0.955) 0.2s, opacity 0.85s cubic-bezier(0.455,  0.030, 0.515, 0.955) 0.2s";
-//         }
-// window.onscroll = function(){
-    
-//     let pos = pageYOffset; // Переменная получает значение Y при прокрутке
-    
-//     if(pos > 500){
-//         animSection2[0].style.opacity = "1";
-//         animSection2[0].style.transform = "translateX(0)";
-//         animSection2[0].style.transition = "transform 0.85s cubic-bezier(0.455,  0.030, 0.515, 0.955) 0.2s, opacity 0.85s cubic-bezier(0.455,  0.030, 0.515, 0.955) 0.2s";
-//     }
-//     if(pos > 1000){
-//         animSection3[0].style.opacity = "1";
-//         animSection3[0].style.transform = "translateX(0)";
-//         animSection3[0].style.transition = "transform 0.85s cubic-bezier(0.455,  0.030, 0.515, 0.955) 0.2s, opacity 0.85s cubic-bezier(0.455,  0.030, 0.515, 0.955) 0.2s";
-//     }
-//     if(pos > 1500){
-//         animSection4[0].style.opacity = "1";
-//         animSection4[0].style.transform = "translateX(0)";
-//         animSection4[0].style.transition = "transform 0.85s cubic-bezier(0.455,  0.030, 0.515, 0.955) 0.2s, opacity 0.85s cubic-bezier(0.455,  0.030, 0.515, 0.955) 0.2s";
-//     }
-//     if(pos > 2000){
-//         animSection5[0].style.opacity = "1";
-//         animSection5[0].style.transform = "translateX(0)";
-//         animSection5[0].style.transition = "transform 0.85s cubic-bezier(0.455,  0.030, 0.515, 0.955) 0.2s, opacity 0.85s cubic-bezier(0.455,  0.030, 0.515, 0.955) 0.2s";
-//     }
+let a = 1;
+let b = 0;
+let c;
 
-// }
+slidesPartners[0].style.display = 'flex';
+let timeId = setInterval(slidesArrow[1].onclick = function(){
+    slidesPartners[b].style.display = 'none';
+    slidesPartners[a].style.display = 'flex';
+    c = b;
+    b = a;
+    a = c;
+}, 4000);
+
+slidesArrow[0].onclick = function(){
+    slidesPartners[a].style.display = 'none';
+    slidesPartners[b].style.display = 'flex';
+    c = b;
+    b = a;
+    a = c;
+};
